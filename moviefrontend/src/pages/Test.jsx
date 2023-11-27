46721df64fb95385b61edbd04243d4fbfab86c8c
Jonas Hagel <53375542+Hagel360@users.noreply.github.com>
2023-11-27 18:29:20 +0100

    Paginator skeleton

--- moviefrontend/src/pages/Test.jsx
+++ moviefrontend/src/pages/Test.jsx
@@ -1,13 +1,47 @@
-import React from "react";
-import HighestRated from "../components/HighestRated";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import TitleData from "../data/title/titleData";
 import UserBookmarks from "../components/UserBookmarks";
 import UserRatings from "../components/UserRatings";
+import Paginator from "../components/Paginator";
+import TitleClient from "../api/titleClient";
 
-const Test = () =>
+const Test = () => {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [titlepageData, setTitlepageData] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const titleClient = new TitleClient();
+                const titlepageData = await titleClient.getTitles();
+                setTitlepageData(titlepageData);
+                console.log(titlepageData);
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                setError(error.message);
+                console.error('error: ', error);
+            }
+        }
+        fetchData();
+    }
+    , []);
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    if (error) {
+        return <p style={{color: 'red'}}>{error}</p>
+    }
+
+    return (
     <>
-        <h1>Test</h1>
-        <UserBookmarks />
-        <UserRatings />
+        <Paginator page={titlepageData}></Paginator>
     </>
+    );
+}
 
 export default Test;
