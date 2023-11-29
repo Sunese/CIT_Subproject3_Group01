85c43f0c1afe6ae530110ca45ff1c9f55c091ed0
Sune Engtorp <suneengtorp@live.dk>
2023-11-29 18:25:09 +0100

    refactor: formatting

--- moviefrontend/src/utils/AuthContext.js
+++ moviefrontend/src/utils/AuthContext.js
@@ -1,66 +1,65 @@
-import React from 'react';
-import { createContext, useContext, useReducer } from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { createContext, useContext, useReducer } from "react";
+import PropTypes from "prop-types";
 
 const AuthContext = createContext();
 
 const initialState = {
-    isAuthenticated: false,
-    token: '',
-    username: ''
+  isAuthenticated: false,
+  token: "",
+  username: "",
 };
 
 const authReducer = (state, action) => {
-    switch (action.type) {
-        case 'LOGIN':
-            console.log('Updating login context for: ', action.payload.username);
-            return {
-                ...state,
-                isAuthenticated: true,
-                token: action.payload.token,
-                username: action.payload.username
-            };
-        case 'LOGOUT':
-            console.log('Updating logout context for: ', state.username);
-            return {
-                ...state,
-                isAuthenticated: false,
-                token: '',
-                username: ''
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "LOGIN":
+      console.log("Updating login context for: ", action.payload.username);
+      return {
+        ...state,
+        isAuthenticated: true,
+        token: action.payload.token,
+        username: action.payload.username,
+      };
+    case "LOGOUT":
+      console.log("Updating logout context for: ", state.username);
+      return {
+        ...state,
+        isAuthenticated: false,
+        token: "",
+        username: "",
+      };
+    default:
+      return state;
+  }
 };
 
 const AuthProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, initialState);
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-    const login = (token, username) => {
-        dispatch({ type: 'LOGIN', payload: { token, username } });
-    };
+  const login = (token, username) => {
+    dispatch({ type: "LOGIN", payload: { token, username } });
+  };
 
-    const logout = () => {
-        dispatch({ type: 'LOGOUT' });
-    };
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
 
-    return (
-        <AuthContext.Provider value={{ ...state, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ ...state, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 AuthProvider.propTypes = {
-    children: PropTypes.any
+  children: PropTypes.any,
 };
 
-
 const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
 
 export { AuthProvider, useAuth };
