565b02f25afbb02c286413af3d71aaadb4450ed5
Sune Engtorp <suneengtorp@live.dk>
2023-12-04 14:45:07 +0100

    fix: update user title rating

--- moviefrontend/src/components/TitleRating.jsx
+++ moviefrontend/src/components/TitleRating.jsx
@@ -12,23 +12,29 @@ import TitleRatingData from "../data/title/titleRatingData";
 const TitleRating = ({ titleId }) => {
   const [rating, setRating] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await TitleClient.getTitleRatings(titleId);
-      if (response.status === 200) {
-        const titleRating = TitleRatingData.fromJson(await response.json());
-        console.log("got title rating: ", titleRating);
-        setRating(titleRating);
-      } else if (response.status === 404) {
-        console.log("no rating found");
-      } else {
-        console.log("error getting rating");
+      try {
+        const response = await TitleClient.getTitleRatings(titleId);
+        if (response.status === 200) {
+          const titleRating = TitleRatingData.fromJson(await response.json());
+          setRating(titleRating);
+        } else {
+          setError("Could not retreive rating");
+        }
+      } catch (error) {
+        setError("Could not retreive rating");
       }
     }
 
+    setLoading(true);
     fetchData();
+    setLoading(false);
   }, [titleId]);
+
+  if (error) return <div style={{ color: "red" }}>{error}</div>;
 
   return (
     <>
@@ -57,7 +63,9 @@ const TitleRating = ({ titleId }) => {
           )}
         </Col>
         <Col xs={2}></Col>
-        <YourRating titleid={titleId} />
+        <Col xs="auto">
+          <YourRating titleid={titleId} />
+        </Col>
       </Row>
     </>
   );
