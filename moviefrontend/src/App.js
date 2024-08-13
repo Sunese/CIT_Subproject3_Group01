5a8f957b02ccd0038872060b20febd042231a4db
Sunese <suneengtorp@live.dk>
2024-08-13 09:35:25 +0200

    add init tauri app and invoke test command

--- moviefrontend/src/App.js
+++ moviefrontend/src/App.js
@@ -16,9 +16,18 @@ import Account from "./pages/Account";
 import SearchHistory from "./pages/SearchHistory";
 import { NotificationProvider } from "./utils/NotificationContext";
 import Ratings from "./pages/Ratings";
+import { invoke } from '@tauri-apps/api'
 
-const App = () => (
-  <AuthProvider>
+const App = () =>
+  {
+  // now we can call our Command!
+  // Right-click the application background and open the developer tools.
+  // You will see "Hello, World!" printed in the console!
+  invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response));
+  
+  return <AuthProvider>
     <NotificationProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
@@ -38,6 +47,6 @@ const App = () => (
       </Routes>
     </NotificationProvider>
   </AuthProvider>
-);
+  };
 
 export default App;
